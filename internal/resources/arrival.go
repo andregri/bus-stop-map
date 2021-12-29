ebc1c1191f01843673024125ef5c6934826d5b5b
andregri <andrea.grillo96@live.com>
2021-12-29 12:16:22 +0100

    implement POST and GET handlers

--- internal/resources/arrival.go
+++ internal/resources/arrival.go
@@ -1,6 +1,12 @@
 package resources
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/andregri/bus-stop-map/internal/dbutils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,12 +17,61 @@ type Arrival struct {
 	Time     string `json:"time"`
 }
 
+// Return details of the arrival resource
 func GetArrival(c *gin.Context) {
-	panic("Not implemented")
+	id := c.Param("id")
+
+	stmt, err := dbutils.DB.Prepare(`
+		SELECT id, stop_code, bus_line, time
+		FROM arrival
+		WHERE id=$1
+	`)
+	if err != nil {
+		log.Println("Error in select statement: ", err)
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		row := stmt.QueryRow(id)
+
+		var arrival Arrival
+		var t time.Time
+		row.Scan(&arrival.ID, &arrival.StopCode, &arrival.BusLine, &t)
+		arrival.Time = t.String()
+		log.Println(arrival)
+
+		c.JSON(http.StatusOK, arrival)
+	}
 }
 
 func CreateArrival(c *gin.Context) {
-	panic("Not implemented")
+	// Decode JSON request body
+	decoder := json.NewDecoder(c.Request.Body)
+	var arrival Arrival
+	err := decoder.Decode(&arrival)
+	log.Println(arrival)
+
+	if err != nil {
+		log.Println("Error in decoding JSON: ", err)
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+
+		newID := 0
+		err = dbutils.DB.QueryRow(`
+			INSERT INTO arrival
+			(stop_code, bus_line, time)
+			VALUES ($1,$2,$3)
+			RETURNING id
+		`, arrival.StopCode, arrival.BusLine, arrival.Time).Scan(&newID)
+		if err != nil {
+			log.Println("Error in insert query: ", err)
+			c.String(http.StatusInternalServerError, err.Error())
+		} else {
+
+			// Return the created ID
+			c.JSON(http.StatusCreated, gin.H{
+				"id": newID,
+			})
+		}
+	}
 }
 
 func DeleteArrival(c *gin.Context) {
